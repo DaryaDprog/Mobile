afe3d340a58c545c111dcf92daad141b91b37718
DaryaDprog <61011966+DaryaDprog@users.noreply.github.com>
2020-02-27 23:58:46 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,22 @@
+$(function(){
+    $(".tabs__option").on("click", function(event) {
+        event.preventDefault();
+        $(".tabs__option").removeClass("active");
+        $(this).addClass("active");
+        $(".tabs__panel").removeClass("active");
+        $(".tabs__panel").eq($(this).index()).addClass("active");
+    });
+
+
+    $("[data-collapse]").on("click", function(event) {
+        $(this).toggleClass("active");
+        event.preventDefault();
+        var blockId = $(this).data('collapse');
+        $(blockId).slideToggle();
+    
+    });
+
+});
+ 
+
+
